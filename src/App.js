1c4446819c6a5e5b17c92fd913ce1a3875dcc538
shashidharbhat000@gmail.com <shashidharbhat000@gmail.com>
2025-01-06 12:25:03 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'; // Import React
+import Quizapp from './Quizapp'; // Import Quizapp component
+
+export default function App() {
+  return (
+    <div>
+      <Quizapp />
+    </div>
+  );
+}
